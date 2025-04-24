c400895a7f91d81f6a95b06886145be73b7e0973
iyersid4 <siyer2405@gmail.com>
2025-04-23 21:51:05 -0400

    Initial Commit for setup. Backend complete with endpoints matching. Endpoints were a bit buggy, so after CORS is set up for the Angular UI, I will look into those later.

---
+++ Backend/TaskTrackerAPI/Data/TaskTrackerData.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskTrackerAPI.Models;
+
+namespace TaskTrackerAPI.Data
+{
+    public class TaskTrackerData : DbContext
+    {
+        public TaskTrackerData(DbContextOptions<TaskTrackerData> options) : base(options)
+        {
+        }
+
+        public DbSet<TaskItem> Tasks { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TaskItem>().HasData(
+                new TaskItem
+                {
+                    Id = 1,
+                    Title = "Test Tesk",
+                    Description = "This is a test.",
+                    DueDate = DateTime.Now,
+                    IsCompleted = false
+                }
+                );
+        }
+    }
+}
