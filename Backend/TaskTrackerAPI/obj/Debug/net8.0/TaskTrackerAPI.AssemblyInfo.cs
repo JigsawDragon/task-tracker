9811beb3860a913c9816cd78903e96c565a1317c
iyersid4 <siyer2405@gmail.com>
2025-04-28 12:03:01 -0400

    Cache change

--- Backend/TaskTrackerAPI/obj/Debug/net8.0/TaskTrackerAPI.AssemblyInfo.cs
+++ Backend/TaskTrackerAPI/obj/Debug/net8.0/TaskTrackerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskTrackerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81a0f464dba6f014560c4e6bc54612dce24bd172")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9363e7cbe88ee9e01a6da4562738d22ca577dc8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskTrackerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskTrackerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
